bdd7a33ac879ffbd211c81afe8dd0851a9dd770f
nehasharma7386 <neha.sharma8@fisglobal.com>
2026-05-08 12:07:25 +0530

    Program updated

--- assignments/python-basics/starter-code.py
+++ assignments/python-basics/starter-code.py
@@ -2,6 +2,10 @@
 def welcome_message():
     # Ask the user for their name, age, and favorite color
     # Return a formatted welcome message
+    name = input("Enter your name:")
+    age = input("Enter your age:")
+    color = input("Enter your favourite color:")
+    print(f"Hello, {name}! You are {age} years old and your favourite color is {color}")
     pass
 
 # Task 2
@@ -10,6 +14,10 @@ def welcome_message():
 def add_two_numbers():
     # Ask the user to enter two numbers
     # Add them together, print and return the result
+    num1 = int(input("Enter first number: "))
+    num2 = int(input("Enter the second number: "))
+    sum = num1 + num2
+    print(f"SUM: {sum}")
     pass
 
 # Task 3
@@ -17,4 +25,13 @@ def add_two_numbers():
 
 def is_even(number):
     # Return True if number is even, False if odd
+    if num %2 ==0:
+        return True
+    else:
+        return False
     pass
+    
+welcome_message()
+add_two_numbers()
+num = int(input("Enter a number: "))
+print(is_even(num))
